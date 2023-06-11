2e1bd013bc13c80b17a63f76cd04ce96dc3ed411
Akn <sibata486@gmail.com>
2023-06-12 01:33:54 +0900

    add / 指定の画像を切り抜くコマンドを実装

--- AlphaImageTrimmer/ViewModels/MainWindowViewModel.cs
+++ AlphaImageTrimmer/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
+using AlphaImageTrimmer.Models;
+using Prism.Commands;
 using Prism.Mvvm;
 
 namespace AlphaImageTrimmer.ViewModels
@@ -13,5 +16,21 @@ namespace AlphaImageTrimmer.ViewModels
         public string Title { get => title; set => SetProperty(ref title, value); }
 
         public List<FileInfo> BitmapFiles { get => bitmapFiles; set => SetProperty(ref bitmapFiles, value); }
+
+        public DelegateCommand CropCommand => new DelegateCommand(() =>
+        {
+            if (BitmapFiles == null || BitmapFiles.Count == 0)
+            {
+                return;
+            }
+
+            foreach (var bf in BitmapFiles)
+            {
+                var bmp = new Bitmap(bf.FullName);
+                var fileName = $@"{bf.DirectoryName}\{Path.GetFileNameWithoutExtension(bf.FullName)}_cut{bf.Extension}";
+                var rect = Trimmer.GetRect(bmp);
+                Trimmer.Crop(bmp, rect, fileName);
+            }
+        });
     }
 }
